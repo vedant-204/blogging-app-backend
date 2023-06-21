e72f44a5e9ed10d73778c0d18e3a59f021f080d2
vedant-204 <katyayana2003@gmail.com>
2023-06-21 14:09:04 +0530

    routes configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,34 @@
+import bodyParser from "body-parser";
 import express from "express";
+import session from "express-session";
+import passport from "passport";
+import { passportAuth } from "./config/jwt-middleware.js";
 import config from "./config/serverConfig.js";
+import apiRoutes from "./routes/index.js";
+
+const app = express()
+
+app.use(session({
+  secret: config.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true3
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passportAuth(passport);
+
+app.use(
+  cors({
+    origin: config.baseUrl ?? "*",
+    credentials: true,
+  })
+);
+
+const startServer = async () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  app.use("/api", apiRoutes);
+}
